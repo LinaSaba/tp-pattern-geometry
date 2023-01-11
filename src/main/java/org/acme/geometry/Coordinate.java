6953cb3356c2a145ceaac784cdb3aa3be564862f
linasaba <linasaba.18@gmail.com>
2023-01-11 15:10:37 +0100

    first push

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -1,5 +1,21 @@
 package org.acme.geometry;
 
 public class Coordinate {
+    private double x;
+    private double y;
 
+    public Coordinate() {
+        this.x = 0.0;
+        this.y = 0.0;
+    }
+
+    public Coordinate(double x, double y) {
+    }
+
+    public double getX(){
+        return this.x;
+    }
+    public double getY(){
+        return this.y;
+    }
 }
